46a29de5d333cea76f449b4b46c73aa1d22ff1b2
Stingeder <72016024+Stingeder@users.noreply.github.com>
2022-06-02 21:55:17 +0200

    Update EnemyStats.cs

--- Lazarus/Assets/Scripts/EnemyStats.cs
+++ Lazarus/Assets/Scripts/EnemyStats.cs
@@ -1,48 +1,28 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
-public class EnemyStats
+public class EnemyStats : MonoBehaviour
 {
     private const int MAX_HEALTH = 90;
-	private const int MIN_HEALTH = 70
+	private const int MIN_HEALTH = 70;
 	private const int MIN_DAMAGE = 2;
 	private const int MAX_DAMAGE = 4;
 	private const int CRIT_DAMAGE = 5;
 	
-	public int health;
-	public int damage;
+	public int _health;
+	public int _damage;
 	
 	public EnemyStats()
 	{
-		Random rnd = new Random();
-		health = rnd.next(MIN_HEALTH, MAX_HEALTH);
-		damage = rnd.next(MIN_DAMAGE, MAX_DAMAGE);
+		_health = Random.Range(MIN_HEALTH, MAX_HEALTH);
+		_damage = Random.Range(MIN_DAMAGE, MAX_DAMAGE);
 	}
 	
-	public int Health()
-	{
-		get
-		{
-			return health;
-		}
-	}
+	public int Health{ get => _health; }
 	
-	public int Damage()
-	{
-		get
-		{
-			return damage;
-		}
-	}
+	public int Damage{ get => _damage; }
 	
-	public int Crit_Damage()
-	{
-		get
-		{
-			return CRIT_DAMAGE;
-		}
-	}
+	public int CritDamage{ get => CRIT_DAMAGE; }
 }
